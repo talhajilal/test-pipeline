ec5bf432461025cf38cd5a941788bdc80b5f5982
talhajilal@gmail.com <Talha.Jilal@Shoppersdrugmart.ca>
2020-03-26 22:50:16 -0400

    groovy

--- first.groovy
+++ first.groovy
@@ -1,6 +1,25 @@
-pipeline{
-  agent any
-  environment {
-    RELEASENAME="yourProject-ci"
-  }
+// Scripted Pipeline
+node ('') {
+    stage('dev') {
+  // get code from our Git repository
+  sh "echo 'Hello World'"
+}
+}
+node {
+    stage('SSH transfer') {
+         steps([$class: 'BapSshPromotionPublisherPlugin']) {
+             sshPublisher(
+                 continueOnError: false, failOnError: true,
+                     publishers: [
+                    sshPublisherDesc(
+                        configName: "docker-build",
+                        verbose: true,
+                        transfers: [
+                            sshTransfer(execCommand: "docker ps -a"),
+                            sshTransfer(sourceFiles: "docker images",)
+                        ]
+                    )
+                     ]
+         }
+    }
 }
